abef436a918f5d873a3d522b2975251a91c6a8af
Viacheslav-ua <atph@ukr.net>
2025-05-25 19:51:47 +0300

    first commit

---
+++ src/main.rs
@@ -0,0 +1,15 @@
+
+enum Priority {
+    Low,
+    Medium,
+    High,
+}
+struct Task {
+    name: String,
+    description: String,
+    priority: Priority,
+}
+
+fn main() {
+    println!("Hello, world!");
+}

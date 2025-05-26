1d7fcb6a324348e0590c314e6ecbf86a2aa43917
Viacheslav-ua <atph@ukr.net>
2025-05-26 15:38:17 +0300

    added impl for TasksManager

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+use core::task;
+use std::{result, vec};
+
 use chrono::{DateTime, Local};
 
 
@@ -39,6 +42,57 @@ impl Task {
         println!("Priority: {}", self.priority.to_string());
         println!("Added on: {}", self.add_time.format("%d-%m-%Y %H:%M:%S"));
         println!("-------------------------");}
+}
+
+struct TasksManager {
+    tasks: Vec<Task>,
+}
+
+impl TasksManager {
+    fn new() -> Self {
+        Self { tasks: vec![] }
+    }
+
+    fn print_tasks(&self) {
+        for task in &self.tasks {
+            task.print_task();
+        }
+    }
+
+    fn add_task(&mut self, task: Task) {
+        self.tasks.push(task);
+    }
+
+    fn remove_task(&mut self, name: &str) -> Result<String, String> {
+        if let Some(index) = self.find_task(name) {
+            self.tasks.remove(index);
+            Ok(format!("Task \"{}\" removed successfully.", name))
+            
+        } else {
+            Err(format!("Task \"{}\" not found.", name))
+        }
+    }
+
+    fn find_task(&self, name: &str) -> Option<usize> {
+        self.tasks.iter().position(|task| task.name == name)
+    }
+
+    fn edit_task(&mut self, name: &str, updated_task: Task) -> Result<String, String> {
+        if let Some(index) = self.find_task(name) {
+            match self.tasks.get_mut(index) {
+                Some(task) => {
+                    task.name = updated_task.name;
+                    task.description = updated_task.description;
+                    task.priority = updated_task.priority;
+                    Ok(format!("Task \"{}\" updated successfully.", task.name))
+                }
+                None => Err("Error borrowing task.".to_owned()),
+                
+            }
+        } else {
+            Err(format!("Task \"{}\" not found.", name))
+        }
+    }
 }
 
 fn main() {
